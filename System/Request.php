a282f1d8004bf6e0cc9b8ec466ccd2bb3989a4e8
tucno21 <carlitostucno@gmail.com>
2022-07-14 15:54:46 -0500

    request get post

--- System/Request.php
+++ System/Request.php
@@ -31,4 +31,39 @@ class Request
         }
         return $path = substr($path, 0, $position);
     }
+
+
+    public function isGet()
+    {
+        return $this->methodWeb() === 'get';
+    }
+
+    public function isPost()
+    {
+        return $this->methodWeb() === 'post';
+    }
+
+    /**
+     * captura los datos de la url mediante el metodo GET y post
+     */
+    public function getInput()
+    {
+        $data = [];
+        if ($this->isGet()) {
+            foreach ($_GET as $key => $value) {
+                $data[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+        if ($this->isPost()) {
+            foreach ($_POST as $key => $value) {
+                $data[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+
+        if (!empty($_FILES)) {
+            $data = array_merge($data, $_FILES);
+        }
+
+        return $data;
+    }
 }
